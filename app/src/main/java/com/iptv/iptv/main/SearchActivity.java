c5f6eb49bb7c82a140b8769a9059f944838b7232
karn <karn358@gmail.com>
2017-03-22 22:48:53 +0700

    initial search

--- app/src/main/java/com/iptv/iptv/main/SearchActivity.java
+++ app/src/main/java/com/iptv/iptv/main/SearchActivity.java
@@ -1,13 +1,20 @@
 package com.iptv.iptv.main;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.iptv.iptv.R;
 
 public class SearchActivity extends LeanbackActivity {
 
     EditText mSearchText;
+    TextView mMovieText;
+    TextView mSeriesText;
+    TextView mLiveText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,35 @@ public class SearchActivity extends LeanbackActivity {
         getWindow().setBackgroundDrawableResource(R.drawable.custom_background);
 
         mSearchText = (EditText) findViewById(R.id.search);
-//        mSearchText.requestFocus();
+        mMovieText = (TextView) findViewById(R.id.movie);
+        mSeriesText = (TextView) findViewById(R.id.series);
+        mLiveText = (TextView) findViewById(R.id.live);
+
+        mSearchText.requestFocus();
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+
+    private void performSearch() {
+
+        initialSelect();
+    }
+
+    private void initialSelect() {
+        mMovieText.setVisibility(View.VISIBLE);
+        mSeriesText.setVisibility(View.VISIBLE);
+        mLiveText.setVisibility(View.VISIBLE);
+
+        mSearchText.clearFocus();
+        mMovieText.requestFocus();
+    }
+
 }

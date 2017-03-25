a45adbb7b4586015c14e620626d85855b21d1886
karn <karn358@gmail.com>
2017-03-25 13:35:23 +0700

    update date&time

--- app/src/main/java/com/iptv/iptv/main/HomeActivity.java
+++ app/src/main/java/com/iptv/iptv/main/HomeActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.iptv.iptv.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class HomeActivity extends LeanbackActivity {
 
@@ -18,6 +22,8 @@ public class HomeActivity extends LeanbackActivity {
     RelativeLayout mSeriesButton;
     RelativeLayout mAdvertiseButton;
     RelativeLayout mSettingButton;
+
+    TextView mDateTimeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,13 @@ public class HomeActivity extends LeanbackActivity {
         mSeriesButton = (RelativeLayout) findViewById(R.id.btn_series);
         mAdvertiseButton = (RelativeLayout) findViewById(R.id.btn_advertise);
         mSettingButton = (RelativeLayout) findViewById(R.id.btn_setting);
+
+        mDateTimeText = (TextView) findViewById(R.id.datetime);
+
+        Thread myThread;
+        Runnable runnable = new CountDownRunner();
+        myThread= new Thread(runnable);
+        myThread.start();
 
         mLiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +79,43 @@ public class HomeActivity extends LeanbackActivity {
                 startActivity(intent, bundle);
             }
         });
-
     }
+
+    public void doWork() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try{
+//                    Date dt = new Date();
+//                    int hours = dt.getHours();
+//                    int minutes = dt.getMinutes();
+//                    int seconds = dt.getSeconds();
+//                    String curTime = hours + ":" + minutes + ":" + seconds;
+
+                    Calendar c = Calendar.getInstance();
+
+                    SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                    String formattedDate = df.format(c.getTime());
+
+                    mDateTimeText.setText(formattedDate);
+                }catch (Exception e) {}
+            }
+        });
+    }
+
+
+    class CountDownRunner implements Runnable{
+        // @Override
+        public void run() {
+            while(!Thread.currentThread().isInterrupted()){
+                try {
+                    doWork();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }catch(Exception e){
+                }
+            }
+        }
+    }
+
 }

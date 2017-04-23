9e8ac9c2558d337483b1edccfb73853b9e185ef8
karn <karn358@gmail.com>
2017-04-23 20:45:16 +0700

    add sport rerun to fav page

--- app/src/main/java/com/iptv/iptv/main/FavoriteGridActivity.java
+++ app/src/main/java/com/iptv/iptv/main/FavoriteGridActivity.java
@@ -9,6 +9,7 @@ import com.iptv.iptv.R;
 import com.iptv.iptv.main.event.LoadLiveEvent;
 import com.iptv.iptv.main.event.LoadMovieEvent;
 import com.iptv.iptv.main.event.LoadSeriesEvent;
+import com.iptv.iptv.main.event.LoadSportEvent;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -17,6 +18,7 @@ public class FavoriteGridActivity extends LeanbackActivity {
     TextView mMovieText;
     TextView mSeriesText;
     TextView mLiveText;
+    TextView mSportText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,7 @@ public class FavoriteGridActivity extends LeanbackActivity {
         mMovieText = (TextView) findViewById(R.id.movie);
         mSeriesText = (TextView) findViewById(R.id.series);
         mLiveText = (TextView) findViewById(R.id.live);
+        mSportText = (TextView) findViewById(R.id.sport);
 
         mMovieText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +55,14 @@ public class FavoriteGridActivity extends LeanbackActivity {
             }
         });
 
+        mSportText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTextSelected(mSportText);
+                setContainerSelected(findViewById(R.id.sport_container));
+            }
+        });
+
         loadFavorites();
     }
 
@@ -62,6 +73,7 @@ public class FavoriteGridActivity extends LeanbackActivity {
                 EventBus.getDefault().post(new LoadMovieEvent(UrlUtil.appendUri(UrlUtil.FAVORITE_URL, UrlUtil.addToken())));
                 EventBus.getDefault().post(new LoadSeriesEvent(UrlUtil.appendUri(UrlUtil.FAVORITE_URL, UrlUtil.addToken())));
                 EventBus.getDefault().post(new LoadLiveEvent(UrlUtil.appendUri(UrlUtil.FAVORITE_URL, UrlUtil.addToken())));
+                EventBus.getDefault().post(new LoadSportEvent(UrlUtil.appendUri(UrlUtil.FAVORITE_URL, UrlUtil.addToken())));
             }
         }, 500);
 
@@ -79,6 +91,7 @@ public class FavoriteGridActivity extends LeanbackActivity {
         mMovieText.setSelected(false);
         mSeriesText.setSelected(false);
         mLiveText.setSelected(false);
+        mSportText.setSelected(false);
 
         currentText.setSelected(true);
     }
@@ -87,6 +100,7 @@ public class FavoriteGridActivity extends LeanbackActivity {
         findViewById(R.id.movie_container).setVisibility(View.GONE);
         findViewById(R.id.series_container).setVisibility(View.GONE);
         findViewById(R.id.live_container).setVisibility(View.GONE);
+        findViewById(R.id.sport_container).setVisibility(View.GONE);
 
         view.setVisibility(View.VISIBLE);
     }

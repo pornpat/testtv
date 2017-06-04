daad100e2847f3cb56b7f7a61b995502168cb513
karn <karn@buzzwoo.de>
2017-06-04 11:44:03 +0700

    show toast on token expired

--- app/src/main/java/com/iptv/iptv/main/MovieGridFragment2.java
+++ app/src/main/java/com/iptv/iptv/main/MovieGridFragment2.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.iptv.iptv.R;
 import com.iptv.iptv.lib.MovieDetailsActivity;
@@ -195,6 +196,7 @@ public class MovieGridFragment2 extends Fragment implements LoaderManager.Loader
     @Subscribe
     public void onTokenError(TokenErrorEvent event) {
         PrefUtil.setStringProperty(R.string.pref_token, "");
+        Toast.makeText(getActivity(), "Token หมดอายุ กรุณาล๊อคอินใหม่อีกครั้ง", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);

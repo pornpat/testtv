27eb066d1cc6233241fa49f4841f34773ecc495c
karn <karn@buzzwoo.de>
2017-06-18 17:04:53 +0700

    add new movie detail

--- app/src/main/java/com/iptv/iptv/main/MovieDetailsActivity.java
+++ app/src/main/java/com/iptv/iptv/main/MovieDetailsActivity.java
@@ -16,24 +16,156 @@ package com.iptv.iptv.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.iptv.iptv.R;
+import com.iptv.iptv.main.data.MovieDataUtil;
+import com.iptv.iptv.main.model.MovieItem;
+import com.iptv.iptv.main.model.TrackItem;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
 
-/*
- * Details activity class that loads LeanbackDetailsFragment class
- */
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+
 public class MovieDetailsActivity extends Activity {
     public static final String SHARED_ELEMENT_NAME = "hero";
     public static final String MOVIE = "Movie";
 
-    /**
-     * Called when the activity is first created.
-     */
+    private MovieItem mSelectedMovie;
+
+    private ImageView mImage;
+    private TextView mEngTitle;
+    private TextView mTitle;
+    private TextView mDetail;
+    private RecyclerView mChoiceList;
+    private RecyclerView mRecommendList;
+
+    private View mLoading;
+    private View mContent;
+
+    private boolean isFav = false;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
         getWindow().setBackgroundDrawableResource(R.drawable.custom_background);
+
+        mSelectedMovie = Parcels.unwrap(getIntent().getParcelableExtra(MovieDetailsActivity.MOVIE));
+
+        mLoading = findViewById(R.id.loading);
+        mContent = findViewById(R.id.layout_content);
+
+        mImage = (ImageView) findViewById(R.id.img);
+        mEngTitle = (TextView) findViewById(R.id.txt_eng_title);
+        mTitle = (TextView) findViewById(R.id.txt_title);
+        mDetail = (TextView) findViewById(R.id.txt_detail);
+        mChoiceList = (RecyclerView) findViewById(R.id.list_choice);
+        mChoiceList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        mRecommendList = (RecyclerView) findViewById(R.id.list_recommend);
+        mRecommendList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        Glide.with(getApplicationContext()).load(mSelectedMovie.getImageUrl()).placeholder(R.drawable.movie_placeholder)
+                .error(R.drawable.movie_placeholder).override(300, 450).centerCrop().listener(new RequestListener<String, GlideDrawable>() {
+            @Override
+            public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                mImage.setImageResource(R.drawable.movie_placeholder);
+                return true;
+            }
+
+            @Override
+            public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                mImage.setImageDrawable(resource);
+                return true;
+            }
+        }).into(mImage);
+
+        mEngTitle.setText(mSelectedMovie.getEngName());
+        mTitle.setText(mSelectedMovie.getName());
+        mDetail.setText(mSelectedMovie.getDescription());
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(UrlUtil.appendUri(UrlUtil.getFavCheckUrl(mSelectedMovie.getId()), UrlUtil.addToken()), new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                showChoice();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                try {
+                    JSONObject jsonObject = new JSONObject(responseString);
+                    isFav = jsonObject.getBoolean("isFavorite");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                showChoice();
+            }
+        });
+
+        if (mSelectedMovie.getType().equals("movie")) {
+            AsyncHttpClient client2 = new AsyncHttpClient();
+            client2.get(UrlUtil.appendUri(UrlUtil.getRecommendUrl(UrlUtil.MOVIE_URL, mSelectedMovie.getId()), UrlUtil.addToken()), new TextHttpResponseHandler() {
+                @Override
+                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                    List<MovieItem> list = MovieDataUtil.getMovieListFromJson(responseString);
+                    mRecommendList.setAdapter(new RecommendAdapter(MovieDetailsActivity.this, list));
+                }
+            });
+        } else if (mSelectedMovie.getType().equals("sport")) {
+            AsyncHttpClient client2 = new AsyncHttpClient();
+            client2.get(UrlUtil.appendUri(UrlUtil.getRecommendUrl(UrlUtil.SPORT_URL, mSelectedMovie.getId()), UrlUtil.addToken()), new TextHttpResponseHandler() {
+                @Override
+                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                    List<MovieItem> list = MovieDataUtil.getMovieListFromJson(responseString);
+                    mRecommendList.setAdapter(new RecommendAdapter(MovieDetailsActivity.this, list));
+                }
+            });
+        }
+    }
+
+    private void showChoice() {
+        List<TrackItem> trackList = mSelectedMovie.getTracks();
+        trackList.add(new TrackItem());
+        mChoiceList.setAdapter(new ChoiceAdapter(MovieDetailsActivity.this, trackList, isFav));
+        mChoiceList.requestFocus();
+
+        showContent(true);
+    }
+
+    private void showContent(boolean show) {
+        if (show) {
+            mLoading.setVisibility(View.GONE);
+            mContent.setVisibility(View.VISIBLE);
+        } else {
+            mLoading.setVisibility(View.VISIBLE);
+            mContent.setVisibility(View.GONE);
+        }
     }
 
 }
---
+++ app/src/main/java/com/iptv/iptv/main/RecommendAdapter.java
@@ -0,0 +1,113 @@
+package com.iptv.iptv.main;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.iptv.iptv.R;
+import com.iptv.iptv.main.model.MovieItem;
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import java.util.List;
+
+/**
+ * Created by Karn on 7/5/2560.
+ */
+
+public class RecommendAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final Context mContext;
+    private final List<MovieItem> mValues;
+
+    public RecommendAdapter(Context context, List<MovieItem> items) {
+        mContext = context;
+        mValues = items;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_recommend, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        final MovieViewHolder vh = (MovieViewHolder) holder;
+        vh.mItem = mValues.get(position);
+        Glide.with(mContext.getApplicationContext()).load(mValues.get(position).getImageUrl()).placeholder(R.drawable.movie_placeholder)
+                .error(R.drawable.movie_placeholder).override(200, 300).centerCrop().listener(new RequestListener<String, GlideDrawable>() {
+            @Override
+            public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                vh.mTitle.setText(mValues.get(position).getName());
+                vh.mEngTitle.setText(mValues.get(position).getEngName());
+                vh.mImage.setImageDrawable(resource);
+                return true;
+            }
+        }).into(vh.mImage);
+
+        vh.mView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean isFocused) {
+                if (isFocused) {
+                    vh.mTitle.setSelected(true);
+                    vh.mEngTitle.setSelected(true);
+                    vh.mImage.setBackground(ContextCompat.getDrawable(mContext, R.drawable.bg_movie_selected));
+                } else {
+                    vh.mTitle.setSelected(false);
+                    vh.mEngTitle.setSelected(false);
+                    vh.mImage.setBackgroundColor(ContextCompat.getColor(mContext, android.R.color.transparent));
+                }
+            }
+        });
+
+        vh.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                EventBus.getDefault().post(new SelectMovieEvent(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+//    @Override
+//    public void onViewDetachedFromWindow(RecyclerView.ViewHolder holder) {
+//        if (holder instanceof MovieViewHolder) {
+//            Glide.clear(((MovieViewHolder) holder).mImage);
+//        }
+//        super.onViewDetachedFromWindow(holder);
+//    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final RoundedImageView mImage;
+        public final TextView mTitle;
+        public final TextView mEngTitle;
+        public MovieItem mItem;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            mImage = (RoundedImageView) itemView.findViewById(R.id.image);
+            mTitle = (TextView) itemView.findViewById(R.id.title);
+            mEngTitle = (TextView) itemView.findViewById(R.id.eng_title);
+        }
+    }
+
+}
